850e5d81c31c14ce1ff72a895189ec28e7c569f6
ckanzarkar <ckanzarkar@cxr.agency>
2023-06-19 19:35:48 +0530

    added admin dashboard template and auth flow updated

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,12 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
     public function AdminDashboard(){
-        return view('admin.admin_dashboard');
+        return view('admin.index');
+
+    }//end method
+    
+    public function AdminLogout(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+        
+        $request->session()->invalidate();
+        
+        $request->session()->regenerateToken();
+        
+        return redirect('/admin/login');
+    }//end method
+   
+    public function AdminLogin(){
+        return view('admin.admin_login');
 
     }//end method
 }
--- routes/web.php
+++ routes/web.php
@@ -34,11 +34,13 @@ require __DIR__.'/auth.php';
 
 Route::middleware(['auth','role:admin'])->group(function(){
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
+
 }); //End group admin middleware
 
 Route::middleware(['auth','role:agent'])->group(function(){
     Route::get('/agent/dashboard', [AgentController::class, 'AgentDashboard'])->name('agent.dashboard');
 }); //End group agent middleware
 
-
+Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
 
