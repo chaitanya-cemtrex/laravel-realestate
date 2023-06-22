06df708c7cae827ee73e8b6074cebf5256f3d8aa
ckanzarkar <ckanzarkar@cxr.agency>
2023-06-22 18:39:14 +0530

    user profile added

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -10,5 +12,12 @@ class UserController extends Controller
     public function Index()
     {
         return view('frontend.index');
+    }
+   
+    public function UserProfile()
+    {
+        $id = Auth::user()->id;
+        $userData = User::find($id);
+        return view('frontend.dashboard.edit_profile', compact('userData'));
     }
 }
